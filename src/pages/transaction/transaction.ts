9a2857c6332f5717aa834568532150cf2040b2e3
dhonisaputra <dhoni.p.saputra@gmail.com>
2018-02-07 08:34:11 +0700

    ''

--- src/pages/transaction/transaction.ts
+++ src/pages/transaction/transaction.ts
@@ -8,150 +8,166 @@ import { HelperProvider } from '../../providers/helper/helper';
 import * as $ from "jquery"
 import * as moment from 'moment';
 
-/**
- * Generated class for the TransactionPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+/*
+* Generated class for the TransactionPage page.
+*
+* See https://ionicframework.com/docs/components/#navigation for more info on
+* Ionic pages and navigation.
+*/
 
 @IonicPage()
 @Component({
-  selector: 'page-transaction',
-  templateUrl: 'transaction.html',
+    selector: 'page-transaction',
+    templateUrl: 'transaction.html',
 })
 export class TransactionPage {
-	items:any=[]
-	original_items:any=[]
-	outlet:number;
-	filter_by:string;
-	filter_input:any;
-	edit_transaction_status:boolean=false;
+    items:any=[]
+    original_items:any=[]
+    outlet:number;
+    filter_by:string="visitor_name";
+    order_by:string='payment_date DESC';
+    filter_input:any;
+    payment_status:number=-1;
+    filter_date_start:string=moment().format('MM/DD/YYYY');
+    filter_date_end:string=moment().format('MM/DD/YYYY');
+    edit_transaction_status:boolean=false;
 
 
-  transaction_params:any = {data:{limit: 20, page: 1}};
-  constructor(public navCtrl: NavController, public navParams: NavParams, private dbLocalProvider:DbLocalProvider, private helper: HelperProvider, private config: ConfigProvider) {
-
-  	this.dbLocalProvider.opendb('outlet')
-    .then((val)=>{
-      this.outlet = val;
-      
-
-      let dataFetch = {
-          online:true,
-          infinite: false,
-          data: { 
-            limit:20,
-            outlet: this.outlet,
-            page: 1,
-            fields: `pay_id,users_outlet,table_id,bank_id,discount_id,payment_method,outlet,payment_nominal,payment_date,visitor_name,payment_date_only,payment_bills,tax_percent,tax_nominal,paid_date,payment_total,paid_nominal,paid_with_bank_nominal,payment_complete_status,payment_complete_note,bills.outlet.pay_id.type.name.price.total.qty.product.detail_id`
-          }
-      }
-
-      if(this.navParams.data.body)
-      {
-        if(this.navParams.data.today == true)
-        {
-          this.navParams.data.body.where['payment_date_only'] = moment().format('YYYY-MM-DD')
-        }
-        dataFetch.data = Object.assign(dataFetch.data, this.navParams.data.body)
-      }
-
-    	this.get_transaction(dataFetch)
-    })
-  }
-
-  ionViewDidLoad() {
-    
-  }
-
-  get_transaction(data:any={data:{}})
-  {
-  	this.transaction_params = data;
-
-  	let url = this.config.base_url('admin/outlet/transaction/get')
-  	return $.post(url, data.data)
-  	.done((res) => {
-
-  		res = !this.helper.isJSON(res)? res : JSON.parse(res); 
-  		if(res.code == 200)
-  		{
-
-	  		if(data.infinite == true)
-	  		{
-	  			this.items = this.items.concat(res.data);
-	  			this.original_items = this.original_items.concat(res.data);
-	  		}else
-	  		{
-	  			this.items = res.data;
-	  			this.original_items = res.data;
-	  		}
-  		}else
-  		{
-  			alert('Error occured while fetching data transaction!')
-  		}
-// this.filter_transaction()
-	  })
-  }
-
-  infinite_scroll(ev:any={})
-  {
-  	this.transaction_params.data.page += 1;
-  	this.transaction_params.infinite = true;
-  	this.get_transaction(this.transaction_params)
-  	.then( ()=>{
-  		if(this.transaction_params.infinite == true && ev.complete)
-  		{
-  			ev.complete()
-  		}
-  	} )
-  }
+    transaction_params:any = {data:{limit: 20, page: 1}};
+    constructor(public navCtrl: NavController, public navParams: NavParams, private dbLocalProvider:DbLocalProvider, private helper: HelperProvider, private config: ConfigProvider) {
+        console.log(this.filter_date_end, this.filter_date_end)
+        this.dbLocalProvider.opendb('outlet')
+        .then((val)=>{
+            this.outlet = val;
 
 
-  filter_transaction(ev:any={})
-  {
+            let dataFetch = {
+                online:true,
+                infinite: false,
+                data: { 
+                    limit:20,
+                    outlet: this.outlet,
+                    page: 1,
+                    join:['table'],
+                    fields: `pay_id,users_outlet,table_id,bank_id,discount_id,payment_method,outlet,payment_nominal,payment_date,visitor_name,payment_date_only,payment_bills,tax_percent,tax_nominal,paid_date,payment_total,paid_nominal,paid_with_bank_nominal,payment_complete_status,payment_complete_note,bills.outlet.pay_id.type.name.price.total.qty.product.detail_id,table_name`
+                }
+            }
 
-  	let val = this.filter_input;
+            if(this.navParams.data.body)
+            {
+                if(this.navParams.data.today == true)
+                {
+                    this.navParams.data.body.where['payment_date_only'] = moment().format('YYYY-MM-DD')
+                }
+                dataFetch.data = Object.assign(dataFetch.data, this.navParams.data.body)
+            }
 
-    // if the value is an empty string don't filter the items
-    if (val && val.trim() != '') {
-
-    	this.transaction_params.infinite = false;
-    	this.transaction_params.data.page = 1;
-
-      	if(this.filter_by == 'visitor_name')
-      	{
-	      	this.transaction_params.data.like = this.transaction_params.data.like? this.transaction_params.data.like : []
-	  		this.transaction_params.data.like = [[this.filter_by, val]]; 
-	  		delete this.transaction_params.data.where; 
-      	}else
-      	{
-	      	this.transaction_params.data.where = this.transaction_params.data.where? this.transaction_params.data.where : {}
-	  		this.transaction_params.data.where[this.filter_by] = val;
-	  		delete this.transaction_params.data.like; 
-      	}
-	    this.get_transaction(this.transaction_params)
-    }else
-    {
-      this.items = this.original_items;
+            this.get_transaction(dataFetch)
+        })
     }
-  	
 
-  }
-    
-  edit_transaction(i, item)
-  {
-    // this.edit_transaction_status = true;
-    this.navCtrl.push(PaymentPage, {
-      previous: 'transaction-page',
-      event: 'transaction.edit',
-      trigger_event: 'transaction.edit',
-      bill: item,
-      receipt_page_params:
-      {
-        can_edit_slide_item: false
-      }
-    })
-  }
+    ionViewDidLoad() {
+
+    }
+
+    get_transaction(data:any={data:{}})
+    {
+        this.transaction_params = data;
+
+        let url = this.config.base_url('admin/outlet/transaction/get')
+        return $.post(url, data.data)
+        .done((res) => {
+
+            res = !this.helper.isJSON(res)? res : JSON.parse(res); 
+            if(res.code == 200)
+            {
+
+                if(data.infinite == true)
+                {
+                    this.items = this.items.concat(res.data);
+                    this.original_items = this.original_items.concat(res.data);
+                }else
+                {
+                    this.items = res.data;
+                    this.original_items = res.data;
+                }
+            }else
+            {
+                alert('Error occured while fetching data transaction!')
+            }
+        })
+    }
+
+    infinite_scroll(ev:any={})
+    {
+        this.transaction_params.data.page += 1;
+        this.transaction_params.infinite = true;
+        this.get_transaction(this.transaction_params)
+        .then( ()=>{
+            if(this.transaction_params.infinite == true && ev.complete)
+            {
+                ev.complete()
+            }
+        } )
+    }
+
+
+    filter_transaction(ev:any={})
+    {
+
+        let val = this.filter_input;
+
+        this.transaction_params.data.where = this.transaction_params.data.where? this.transaction_params.data.where : {}
+        this.transaction_params.infinite = false;
+        this.transaction_params.data.page = 1;
+
+        if (val && val.trim() != '') {
+
+
+            if(this.filter_by == 'visitor_name')
+            {
+                this.transaction_params.data.like = this.transaction_params.data.like? this.transaction_params.data.like : []
+                this.transaction_params.data.like = [[this.filter_by, val]]; 
+                delete this.transaction_params.data.where[this.filter_by] 
+            }else
+            {
+                this.transaction_params.data.where[this.filter_by] = val;
+                delete this.transaction_params.data.like; 
+            }
+        }
+
+        if(this.filter_by == 'payment_date')
+        {
+            this.transaction_params.data.where['"payment_date >="'] = this.filter_date_start
+            this.transaction_params.data.where['"payment_date <="'] = this.filter_date_end;
+        }
+
+        this.transaction_params.data.where['payment_complete_status'] = this.payment_status;
+        if(this.payment_status < 0)
+        {
+            delete this.transaction_params.data.where['payment_complete_status'];
+        }
+
+        this.transaction_params.data.order_by = this.order_by;
+        console.log(this.transaction_params.data)
+
+        this.get_transaction(this.transaction_params)
+
+
+    }
+
+    edit_transaction(i, item)
+    {
+        this.navCtrl.push(PaymentPage, {
+            previous: 'transaction-page',
+            event: 'transaction.edit',
+            trigger_event: 'transaction.edit',
+            bill: item,
+            receipt_page_params:
+            {
+                can_edit_slide_item: false
+            }
+        })
+    }
 
 }
